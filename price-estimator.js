96f43830796a9ba1d414e4d9550821f850556125
johnoro <officialjohnorourke@gmail.com>
2019-03-14 12:21:58 -0400

    Adds alt estimator

--- price-estimator.js
+++ price-estimator.js
@@ -1,24 +1,46 @@
 const cheerio = require("cheerio");
 const request = require("request-promise");
 
-module.exports = async property => {
-  // map fields between what this API expects and what we have
-  const you_will_conform = {
-    beds: property.bedrooms,
-    baths: property.bathrooms,
-    square_feet: property.sqFt,
-    lot_size: property.lotSize,
-    hoa_per_month: property.hoa,
-    property_types: property.type,
-    property_age: property.age,
-    zip_codes: property.zipcode
-  };
+module.exports = {
+  estimate: async property => {
+    // map fields between what this API expects and what we have
+    const you_will_conform = {
+      beds: property.bedrooms,
+      baths: property.bathrooms,
+      square_feet: property.sqFt,
+      lot_size: property.lotSize,
+      hoa_per_month: property.hoa,
+      property_types: property.type || 0,
+      property_age: property.age,
+      zip_codes: property.zipcode
+    };
 
-  // for some unholy reason they want multipart data
-  return await request
-    .post("https://house-estimator-69.herokuapp.com/", {
-      formData: you_will_conform,
-      transform: cheerio.load
-    })
-    .then($ => $("h2").text());
+    // for some unholy reason they want multipart data
+    return await request
+      .post("https://house-estimator-69.herokuapp.com/", {
+        formData: you_will_conform,
+        transform: cheerio.load
+      })
+      .then($ => $("h2").text());
+  },
+  altEstimate: async property => {
+    const you_will_conform = {
+      year_assessment: property.yearAssessed,
+      land_use_type: 31,
+      beds: property.bedrooms,
+      baths: property.bathrooms,
+      total_rooms: property.rooms,
+      zip: property.zipcode,
+      assessed_property_taxes: property.taxes || ,
+      year_built: new Date().getFullYear() - property.age,
+      sqft_house: property.sqFt
+    };
+
+    return await request
+      .post("https://manjula-regressor.herokuapp.com/", {
+        formData: you_will_conform,
+        transform: cheerio.load
+      })
+      .then($ => $("h2").text());
+  }
 };
